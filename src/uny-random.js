74ed0794e347022ae3e9af7735aa9fed8a01db6c
MoatShrimp <mathias.viklund@gmail.com>
2023-09-30 03:17:08 +0200

    feat: implement base for .colorHSV

--- src/uny-random.js
+++ src/uny-random.js
@@ -251,10 +251,33 @@ export class UnyRandom {
    * @see {@link https://docs.unity3d.com/ScriptReference/Random.ColorHSV.html UnityEngine.Random.ColorHSV}
    */
   colorHSV() {
+    const h = value(this.next);
+    const s = value(this.next);
+    const v = value(this.next);
+
+    const i = Math.floor(h * 6);
+    const f = h * 6 - i;
+    const p = v * (1 - s);
+    const q = v * (1 - f * s);
+    const t = v * (1 - (1 - f) * s);
+
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    switch (i % 6) {
+      case 0: r = v; g = t; b = p; break;
+      case 1: r = q; g = v; b = p; break;
+      case 2: r = p; g = v; b = t; break;
+      case 3: r = p; g = q; b = v; break;
+      case 4: r = t; g = p; b = v; break;
+      case 5: r = v; g = p; b = q; break;
+  }
+
     return {
-      r: 0,
-      g: 0,
-      b: 0,
+      r: roundTo7(r),
+      g: roundTo7(g),
+      b: roundTo7(b),
       a: 0
     }
   };
