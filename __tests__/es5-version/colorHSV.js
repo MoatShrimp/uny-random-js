b0f93fd8b906cffcd04d467b18593d020c1c9ad6
MoatShrimp <mathias.viklund@gmail.com>
2023-09-30 18:03:31 +0200

    feat: add es5 version for older enviroments

---
+++ __tests__/es5-version/colorHSV.js
@@ -0,0 +1,129 @@
+import unyRandom from "../../src/uny-random-es5"; 
+
+describe('.colorHSV vs. UnityEngine.Random.ColorHSV', () => {
+
+  test.each([
+    { init: 0, expected: { r: 0.280165, g: 0.4749825, b: 0.6736069 } },
+    { init: 2147483647, expected: { r: 0.4188507, g: 0.4194092, b: 0.388662 } },
+    { init: 3351175, expected: { r: 0.7824091, g: 0.3163231, b: 0.3163231 } },
+    { init: 7597963, expected: { r: 0.9760998, g: 0.3332019, b: 0.3332019 } },
+    { init: 96438, expected: { r: 0.2132251, g: 0.2335552, b: 0.5398842 } },
+    { init: 4919044, expected: { r: 0.3074178, g: 0.2367011, b: 0.9625161 } },
+    { init: 11288839, expected: { r: 0.7647283, g: 0.7155517, b: 0.7255623 } },
+    { init: 77823943, expected: { r: 0.1020132, g: 0.06225326, b: 0.06371301 } },
+    { init: 1218593819, expected: { r: 0.02201884, g: 0.06335761, b: 0.121665 } },
+    { init: 1585133052, expected: { r: 0.7362407, g: 0.1090128, b: 0.7145579 } },
+    { init: 1658364365, expected: { r: 0.05228301, g: 0.05010686, b: 0.08307291 } },
+  ])('should generate the same Color (error margin 0.00005%) - [Seed: $init]', ({ init, expected }) => {
+
+    // Arrange
+    const rand = unyRandom(init);
+
+    // Act
+    const result = rand.colorHSV();
+
+    // Assert
+    expect(result.r).toBeCloseTo(expected.r, 6);
+    expect(result.g).toBeCloseTo(expected.g, 6);
+    expect(result.b).toBeCloseTo(expected.b, 6);
+  });
+
+  test.each([
+    {
+      init: 4919044,
+      hueMin: 0.4112355,
+      hueMax: 0.9567251,
+      saturationMin: 0.5220811,
+      saturationMax: 0.8733672,
+      valueMin: 0.03283918,
+      valueMax: 0.6109516,
+      expected: { r: 0.4513241, g: 0.1255293, b: 0.5892817 }
+    },
+    {
+      init: 77823943,
+      hueMin: 0.1589235,
+      hueMax: 0.8536462,
+      saturationMin: 0.3470565,
+      saturationMax: 0.7604165,
+      valueMin: 0.5185792,
+      valueMax: 0.9612151,
+      expected: { r: 0.5637339, g: 0.2772641, b: 0.5361265 }
+    },
+    {
+      init: 1218593819,
+      hueMin: 0,
+      hueMax: 0.2928473,
+      saturationMin: 0.5587336,
+      saturationMax: 1,
+      valueMin: 0,
+      valueMax: 1,
+      expected: { r: 0.1160788, g: 0.121665, b: 0.009716179 }
+    },
+  ])('should generate the same Color (error margin 0.00005%) when given ranges - [Seed: $init]',
+    ({ init, hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax, expected }) => {
+
+      // Arrange
+      const rand = unyRandom(init);
+
+      // Act
+      const result = rand.colorHSV(hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax);
+
+      // Assert
+      expect(result.r).toBeCloseTo(expected.r, 6);
+      expect(result.g).toBeCloseTo(expected.g, 6);
+      expect(result.b).toBeCloseTo(expected.b, 6);
+    });
+
+  test.each([
+    {
+      init: 96438,
+      hueMin: 0.4776986,
+      hueMax: 0.5658471,
+      saturationMin: 0.1417581,
+      saturationMax: 0.7545162,
+      valueMin: 0.2890312,
+      valueMax: 0.8724657,
+      alphaMin: 0.09216214,
+      alphaMax: 0.9967413,
+      expected: { r: 0.294453, g: 0.5379881, b: 0.6040183, a: 0.8713523 }
+    },
+    {
+      init: 1585133052,
+      hueMin: 0.3103028,
+      hueMax: 0.6900989,
+      saturationMin: 0.2188076,
+      saturationMax: 0.4164305,
+      valueMin: 0.1378517,
+      valueMax: 0.7215825,
+      alphaMin: 0.09216214,
+      alphaMax: 0.9967413,
+      expected: { r: 0.3478539, g: 0.3975368, b: 0.5676181, a: 0.2574574 }
+    },
+    {
+      init: 1658364365,
+      hueMin: 0.1839423,
+      hueMax: 0.8481582,
+      saturationMin: 0.3851734,
+      saturationMax: 1,
+      valueMin: 0,
+      valueMax: 0.06329382,
+      alphaMin: 0,
+      alphaMax: 1,
+      expected: { r: 0.001949894, g: 0.002597082, b: 0.005258002, a: 0.05327214 }
+    },
+  ])('should generate the same Color (error margin 0.00005%) when ranges, with alpha range [r:$expected.r, g:$expected.g, b:$expected.b] - [Seed: $init]',
+    ({ init, hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax, expected, alphaMin, alphaMax }) => {
+
+      // Arrange
+      const rand = unyRandom(init);
+
+      // Act
+      const result = rand.colorHSV(hueMin, hueMax, saturationMin, saturationMax, valueMin, valueMax, alphaMin, alphaMax);
+
+      // Assert
+      expect(result.r).toBeCloseTo(expected.r, 6);
+      expect(result.g).toBeCloseTo(expected.g, 6);
+      expect(result.b).toBeCloseTo(expected.b, 6);
+      expect(result.a).toBeCloseTo(expected.a, 6);
+    });
+});
---
+++ __tests__/es5-version/value.js
@@ -0,0 +1,29 @@
+import unyRandom from "../../src/uny-random-es5"; 
+
+describe('.value vs. UnityEngine.Random.value', () => {
+  
+  test.each([
+    { init: 0,          expected: [0.5841396, 0.5840824,  0.6736069,  0.766507,   0.3050319] },
+    { init: 2147483647, expected: [0.169694,  0.07331086, 0.4194092,  0.9295188,  0.1002243] },
+    { init: 3351175,    expected: [0,         0.5957063,  0.7824091,  0.5570976,  0.2050821] },
+    { init: 7597963,    expected: [1,         0.6586395,  0.9760998,  0.6824545,  0.8340771] },
+    { init: 96438,      expected: [0.6562939, 0.605054,   0.5398842,  0.8613842,  0.6603977] },
+    { init: 4919044,    expected: [0.6829051, 0.7540809,  0.9625161,  0.1100732,  0.4889563] },
+    { init: 11288839,   expected: [0.9660726, 0.06430603, 0.7647283,  0.2359069,  0.9815067] },
+    { init: 77823943,   expected: [0.993881,  0.3897532,  0.1020132,  0.4291891,  0.6358299] },
+    { init: 1218593819, expected: [0.5975241, 0.8190207,  0.121665,   0.138771,   0.6793048] },
+    { init: 1585133052, expected: [0.8390949, 0.8519332,  0.7362407,  0.1827316,  0.9954406] },
+    { init: 1658364365, expected: [0.6776686, 0.3968328,  0.08307291, 0.05327214, 0.2266419] },
+  ])('should generate the same first $expected.length [0, 1] floats (error margin 0.00005%) - [Seed: $init]', ({ init, expected }) => {
+
+    // Arrange
+    const rand = unyRandom(init);
+
+    for (const int of expected) {
+      // Act
+      const result = rand.value;
+      // Assert
+      expect(result).toBeCloseTo(int, 6);
+    }
+  });
+});
